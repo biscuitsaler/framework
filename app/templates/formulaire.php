f84785dd9523f29a85df4092cb8718d5e6679289
biscuitsaler <durandmallory@live.fr>
2017-01-10 16:48:50 +0100

    ajout formulaire

---
+++ app/templates/formulaire.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    <h1>Mon compte</h1>
+        <form action="/ma-page-de-traitement" method="post">
+    <div>
+        <label for="nom">Nom :</label>
+        <input type="text" id="nom" />
+    </div>
+     <div>
+        <label for="nom">Prenom:</label>
+        <input type="text" id="prenom" />
+    </div>
+    <div>
+        <label for="email">email :</label>
+        <input type="email" id="email" />
+    </div>
+        </form>
+    </body>
+</html>
+
+
+   
